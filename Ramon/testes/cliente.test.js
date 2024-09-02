09f0e0d272803c773227b41b42bcf28b2185af92
Douglas Aurélio DE JESUS <163046084+D0ugla5@users.noreply.github.com>
2024-09-02 17:03:00 -0300

    Add files via upload

--- Ramon/testes/cliente.test.js
+++ Ramon/testes/cliente.test.js
@@ -10,4 +10,33 @@ describe('GET /clientes', () => {
         const res = await request(app).get('/clientes/').send();
         expect(res.body).toBeDefined();
     })
+})  
+describe('POST /clientes', () => {
+    it('criar cliente com sucesso', async () => {
+        const rest = await request(app).post('/clientes/').send(
+            {
+                nome:'Douglasssassas',
+                email:'BD@texte.com',
+                senha:'DbBd'
+            }
+        );
+        expect(rest.status).toBe(204);
+    })
+})
+
+describe('Atualizar /clientes/:id', () => {
+     it('Atualizar nome do cliente com sucesso', async () => {
+        const res = await request(app).post('/clientes/355925f2-40d2-4869-a3a5-8df97eb30c36').send(
+            {
+                nome:'Jão Vitor'
+            })
+            expect(res.status).toBe(201)
+     })
+}) 
+
+describe('Deletar /clientes/:id', () => {
+    it('Cliente delado com sucesso', async () => {
+       const res = await request(app).delete('/clientes/355925f2-40d2-4869-a3a5-8df97eb30c36').send()
+        expect(res.status).toBe(404)
+    })
 })
