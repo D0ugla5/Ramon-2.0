234a40532f499155129b6338d4caf267189015e1
Douglas Aurélio DE JESUS <163046084+D0ugla5@users.noreply.github.com>
2024-09-23 16:51:02 -0300

    Add files via upload
    Documentação feita

--- Ramon/index.js
+++ Ramon/index.js
@@ -1,17 +1,28 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const rota_produto = require('./rotas/rotas-produtos')
 const rota_cliente = require('./rotas/rotas-cliente')
 const rota_auth = require('./rotas/rotas-autenticacao')
+const cookiePaster = require('cookie-parser')
+
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+
+const swaggerDocument = YAML.load('./docs/documentacao.yaml')
 
 app.use(bodyParser.json())
+
+app.use(cookiePaster())
 
 app.use('/produtos', rota_produto)
 
 app.use('/clientes', rota_cliente)
 
 app.use('/auth', rota_auth)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(8721) //bootstrap
 
